f4fe9a31344b0ee7c5b17553c062a1f4e51e6c64
Ozgun Yildiz <ozgun.yildiz@hotmail.de>
2022-04-06 19:05:14 +0200

    Added LC 26 Remove Duplicates from Sorted Array

--- Leetcode.playground/Pages/Two Pointers.xcplaygroundpage/Contents.swift
+++ Leetcode.playground/Pages/Two Pointers.xcplaygroundpage/Contents.swift
@@ -37,21 +37,57 @@ func twoSum2II(_ nums: [Int], _ target: Int) -> [Int] {
 // MARK: - Two Sum 1
 
 func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-        var dict = [Int: Int]()
-        
-        for (i, num) in nums.enumerated() { // 0,2-1,7
-            if let index = dict[target-num] { // 0
-                if i != index {
-                    return [index, i] 
-                }
-            }
-            dict[num] = i // [2:0]
+    var dict = [Int: Int]()
+    for index in 0..<nums.count {
+        if let found = dict[target - nums[index]] {
+            return [found, index]
+        } else {
+            dict[nums[index]] = index
         }
-        return[0]
     }
+    return []
+    }
+
+func twoSum2(_ nums: [Int], _ target: Int) -> [Int] {
+        var dict = [Int:Int]()
+        for (i, n) in nums.enumerated() {
+            if let last = dict[target - n] {
+                return [last, i]
+            }
+            dict[n] = i
+        }
+        return []
+    }
+
+// MARK: - Remove Duplicates from Sorted Array 26
+
+func removeDuplicates(nums: inout [Int]) -> Int {
+        guard nums.count != 0 && nums.count != 1 else { return nums.count }
+        var i = 0
+        for j in 0..<nums.count {
+          if nums[i] != nums[j] {
+            i += 1
+            nums[i] = nums[j]
+          }
+        }
+        return i+1
+    }
+
 
 class Tests: XCTestCase {
     
+    func test26() {
+        var intArray = [1,1,2]
+        let value = removeDuplicates(nums: &intArray)
+        XCTAssertEqual(value, 2)
+    }
+    
+    func test26_1() {
+        var intArray = [0,0,1,1,1,2,2,3,3,4]
+        let value = removeDuplicates(nums: &intArray)
+        XCTAssertEqual(value, 5)
+    }
+
     func test1() {
         let value = twoSum([2,7,11,15], 9)
         XCTAssertEqual(value, [0,1])
@@ -59,7 +95,7 @@ class Tests: XCTestCase {
     
     func test1_2() {
         let value = twoSum([3,2,4], 6)
-        XCTAssertEqual(value, [0,1])
+        XCTAssertEqual(value, [1,2])
     }
     
     func test1_3() {
