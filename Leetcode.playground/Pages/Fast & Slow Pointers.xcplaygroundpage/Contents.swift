dea63d87bb58237646cfbabdceb05d00b355c40b
Ozgun Yildiz <ozgun.yildiz@hotmail.de>
2022-04-14 14:41:20 +0200

    Added Linked List Cycle II 142

--- Leetcode.playground/Pages/Fast & Slow Pointers.xcplaygroundpage/Contents.swift
+++ Leetcode.playground/Pages/Fast & Slow Pointers.xcplaygroundpage/Contents.swift
@@ -1,7 +1,6 @@
 
 import Foundation
 
-// MARK: - Linked List Cycle 141
 
 class ListNode {
      var val: Int
@@ -10,6 +9,8 @@ class ListNode {
         self.val = val
         self.next = nil
     }
+    
+    // MARK: - Linked List Cycle 141
     // - Complexity:
     //   - time: O(n), where n is the number of nodes in the linked list.
     //   - space: O(1), only constant space is used.
@@ -26,6 +27,28 @@ class ListNode {
         }
         
         return false
+    }
+    
+    // MARK: - Linked List Cycle II 142
+    
+    func detectCycle(_ head: ListNode?) -> ListNode? {
+        guard head?.next != nil else { return nil }
+        var slow = head
+        var fast = head
+
+        while slow?.next != nil || fast?.next?.next != nil {
+            slow = slow?.next
+            fast = fast?.next?.next
+            guard slow !== fast else { break }
+        }
+
+        slow = head
+        while slow !== fast {
+            slow = slow?.next
+            fast = fast?.next
+        }
+
+        return slow
     }
 }
 
