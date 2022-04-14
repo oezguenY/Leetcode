4711c35e1eff83d0ae839fe20bcd277463ae6391
Ozgun Yildiz <ozgun.yildiz@hotmail.de>
2022-04-14 19:02:41 +0200

    Added Palindrome Linked List 234

--- Leetcode.playground/Pages/Fast & Slow Pointers.xcplaygroundpage/Contents.swift
+++ Leetcode.playground/Pages/Fast & Slow Pointers.xcplaygroundpage/Contents.swift
@@ -63,6 +63,46 @@ class ListNode {
         
         return slow
     }
+    
+    // MARK: - Palindrome Linked List
+    // 
+    func isPalindrome(_ head: ListNode?) -> Bool {
+            var fast: ListNode? = head
+            var slow: ListNode? = head
+            
+            while fast != nil && fast?.next != nil {
+                fast = fast?.next?.next
+                slow = slow?.next
+            }
+            if fast != nil {
+                slow = slow?.next
+            }
+            slow = reverse(slow)
+            fast = head
+            while slow != nil {
+                if fast?.val != slow?.val {
+                    return false
+                }
+                fast = fast?.next
+                slow = slow?.next
+            }
+            
+            return true
+        }
+        
+        private func reverse(_ head: ListNode?) -> ListNode? {
+            var head = head
+            var pre: ListNode? = nil
+        
+            while head != nil {
+                let next = head?.next
+                head?.next = pre
+                pre = head
+                head = next
+            }
+            
+            return pre
+        }
 }
 
 // MARK: - Happy Number 202
