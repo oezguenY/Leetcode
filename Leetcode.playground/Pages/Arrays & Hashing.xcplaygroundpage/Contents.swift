b3e2c8fdee85a565df645dcfc412d757f08b2014
Ozgun Yildiz <ozgun.yildiz@hotmail.de>
2022-04-19 13:09:45 +0200

    Added Top K Frequent Elements 347

--- Leetcode.playground/Pages/Arrays & Hashing.xcplaygroundpage/Contents.swift
+++ Leetcode.playground/Pages/Arrays & Hashing.xcplaygroundpage/Contents.swift
@@ -13,8 +13,8 @@ func isAnagram(_ s: String, _ t: String) -> Bool {
     }
     return false
     
-    }
-    
+}
+
 
 var string1 = "car"
 var string2 = "rac"
@@ -43,21 +43,21 @@ func isAnagram2(_ s: String, _ t: String) -> Bool {
     }
     return false
     
-    }
+}
 
 isAnagram2("car", "rac")
 
 
 func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-  var dict = [Int:Int]()
-  
-  for (i,num) in nums.enumerated() {
-      if let index = dict[target - num] { // 0
-          return [index, i]
-      }
-      dict[num] = i // [2:0,]
-  }
-  return []
+    var dict = [Int:Int]()
+    
+    for (i,num) in nums.enumerated() {
+        if let index = dict[target - num] { // 0
+            return [index, i]
+        }
+        dict[num] = i // [2:0,]
+    }
+    return []
 }
 twoSum([3,2,4], 6)
 
@@ -71,7 +71,34 @@ func groupAnagrams(_ strs: [String]) -> [[String]] {
         dict[abc] == nil ? dict[abc] = [c] : dict[abc]?.append(c)
     }
     return dict.map({$1})
-    }
+}
 
 
 groupAnagrams(["eat","tea","tan","ate","nat","bat"])
+
+// MARK: - Top K Frequent Elements 347
+
+func topKFrequent(_ nums: [Int], _ k: Int) -> [Int] {
+    
+    var hash = [Int:Int]()
+    var result = [Int]()
+    
+    for num in nums {
+        if let _ = hash[num] {
+            hash[num,default: 0] += 1
+        } else {
+            hash[num] = 1
+        }
+    }
+   
+    let highest = hash.sorted { $0.value > $1.value }.prefix(k)
+    
+    for high in highest {
+        result.append(high.key)
+    }
+
+    return result
+}
+
+topKFrequent([1,1,1,2,2,3], 2)
+
