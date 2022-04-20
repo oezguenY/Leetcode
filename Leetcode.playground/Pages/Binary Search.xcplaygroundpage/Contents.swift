ccc2954112812bf5715d1846413330926ec26743
Ozgun Yildiz <ozgun.yildiz@hotmail.de>
2022-04-20 23:43:27 +0200

    Added 875. Koko Eating Bananas

--- Leetcode.playground/Pages/Binary Search.xcplaygroundpage/Contents.swift
+++ Leetcode.playground/Pages/Binary Search.xcplaygroundpage/Contents.swift
@@ -15,7 +15,7 @@ func search(_ nums: [Int], _ target: Int) -> Int {
 search([-1,0,3,5,9,12], 9)
 
 func search2(_ nums: [Int], _ target: Int) -> Int {
-   
+    
     var left = 0, right = nums.count - 1
     
     while left <= right {
@@ -32,3 +32,92 @@ func search2(_ nums: [Int], _ target: Int) -> Int {
 }
 
 search2([-1,0,3,5,9,12], 9)
+
+// MARK: - 74. Search a 2D Matrix
+
+func searchMatrix(_ matrix: [[Int]], _ target: Int) -> Bool {
+    
+    let oneD = matrix.flatMap { $0 }
+    var left = 0, right = oneD.count - 1
+    
+    while left <= right {
+        let middle = (left + right) / 2
+        if oneD[middle] < target {
+            left = middle + 1
+        } else if oneD[middle] > target {
+            right = middle - 1
+        } else {
+            return true
+        }
+    }
+    return false
+}
+
+
+searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3)
+
+// MARK: - 875. Koko Eating Bananas
+
+/*
+ Question: How many bananas has koko to eat minimum in an hour so she can eat all the piles?
+ Pseudocode:
+ - 1) the minimum answer has to be 1. Because if its below one, koko eats no bananas per hour
+ - 2) Koko cant eat more than 1 pile of bananas per hour
+ - the conclusion from 2) is that the maximum number of bananas koko can eat per hour is the maximum pile. Even if she could eat 100x the amount of bananas per hour, she can only finish one pile of bananas per hour.
+ - the logical conclusion from 1) and 2) are that koko can eat 1...max(piles)
+ - this range contains the different quantities of bananas koko can eat in one hour, so she finishes all the piles in the set amount of hours
+ - we are looking for the MINIMUM amount of bananas koko can eat in an hour, so she finishes all the piles in the set time
+ - our range is sorted (lowest to highest)
+ - say we picked the middle value from our range. Say the value (for the amount of bananas eaten in an hour) we picked is so high, that koko finishes way faster than the set number of hours. This value is a false answer, because we want the MINIMUM number of bananas eaten by koko in an hour so she finishes in the set amount of time.
+ */
+
+// THIS SOLUTION IS UNOPTIMIZED
+
+func minEatingSpeed(_ piles: [Int], _ h: Int) -> Int {
+    guard piles.count > 0 else { return -1 }
+    let range = Array(1...piles.max()!)
+    var l = 0, r = range.count - 1
+    
+    while l <= r {
+        let m = (l + r) / 2 // 5, 2, 3
+        let hours = piles.map( { Double(ceil(Double($0) / Double(range[m])))} ).reduce(0,+) // gives us the amount of hours koko needs to eat all piles given a specific speed (amount of bananas she eats in an hour) // range[m] = 6 // [1,1,2,2]
+        // range[m] = 3 // [1,2,3,4]
+        // range[m] = 4 // [1,2,2,3]
+        if Int(hours) < h { // 6 < 8
+            r = m - 1 // 4
+        } else if Int(hours) > h { // 10 > 8
+            l = m + 1 // 3
+        } else {
+            return range[m]
+        }
+    }
+    return -1
+}
+
+minEatingSpeed([3,6,7,11], 8)
+
+// THIS IS OPTIMIZED
+
+func minEatingSpeed2(_ piles: [Int], _ h: Int) -> Int {
+    
+    var l = 1, r = piles.max()!// r = 11
+    var result = r
+    while l <= r { // 1 <= 11
+        let m = (l + r) / 2 // 6
+        var hours = 0.0
+        for p in piles {
+            hours += Double(ceil(Double(p) / Double(m))) //[1,1,2,2]
+        }
+       
+        if Int(hours) <= h { // 6 < 8
+            result = min(result, m) // 6
+            r = m - 1 // 5
+        } else {
+            l = m + 1
+        }
+    }
+    return result
+}
+
+
+minEatingSpeed2([3,6,7,11], 8)
