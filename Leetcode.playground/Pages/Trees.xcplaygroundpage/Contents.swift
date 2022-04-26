917d4800c647775bf54862c24317896f2c18918c
Ozgun Yildiz <ozgun.yildiz@hotmail.de>
2022-04-26 12:04:32 +0200

    104. Maximum Depth of Binary Tree with BFS

--- Leetcode.playground/Pages/Trees.xcplaygroundpage/Contents.swift
+++ Leetcode.playground/Pages/Trees.xcplaygroundpage/Contents.swift
@@ -33,7 +33,29 @@ class TreeNode {
     func maxDepthDFS(_ root: TreeNode?) -> Int {
         return root != nil ? 1 + max(self.maxDepthDFS(root?.left), self.maxDepthDFS(root?.right)) : 0
     }
-
+    // for every level in the tree, we add the nodes in a queue, loop and add 1 to the level
+    func maxDepthBFS(_ root: TreeNode?) -> Int {
+            guard let root = root else { return 0 }
+            var maxLevel = 0
+            var queue = [root] // 3
+            while !queue.isEmpty {
+                maxLevel += 1 // 1, 2, 3
+                let count = queue.count // 1, 2, 2
+                for _ in 0..<count { // 0, 0, 1, 0, 1
+                    let curr = queue.removeFirst() // []
+                    if let left = curr.left {
+                        queue.append(left) // []
+                    }
+                    if let right = curr.right {
+                        queue.append(right) // []
+                    }
+                }
+            }
+            return maxLevel
+        }
+    
+    
+    
 }
 
 
